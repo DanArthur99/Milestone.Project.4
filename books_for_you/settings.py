56f4dd43408ea6031c20a098c3927dccf19e621a
Dan Arthur <148535574+DanArthur99@users.noreply.github.com>
2024-11-29 00:58:31 +0000

    fix: updated allowed hosts

--- books_for_you/settings.py
+++ books_for_you/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['books-for-you.herokuapp.com', '']
+ALLOWED_HOSTS = ['books-for-you-19d767033b76.herokuapp.com']
 
 
 AUTHENTICATION_BACKENDS = [ 
