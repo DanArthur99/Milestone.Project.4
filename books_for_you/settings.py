f69f0d43efb2a8babfff641de7745e2caa22d0eb
Dan Arthur <148535574+DanArthur99@users.noreply.github.com>
2024-11-29 04:32:31 +0000

    static files

--- books_for_you/settings.py
+++ books_for_you/settings.py
@@ -190,6 +190,12 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticCloudinaryStorage'
 
 
