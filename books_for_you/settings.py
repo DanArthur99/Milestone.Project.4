cceb8d77306435214db0dcb9e950dee6ae6db6fa
Dan Arthur <148535574+DanArthur99@users.noreply.github.com>
2024-11-30 11:30:45 +0000

    feat: cloudinary storage

--- books_for_you/settings.py
+++ books_for_you/settings.py
@@ -189,7 +189,7 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STATICILES_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 
