6d9b2d2c6adcc9ea674e1e0a4770b54c26e3bcca
Dan Arthur <148535574+DanArthur99@users.noreply.github.com>
2024-11-30 12:48:34 +0000

    fix: profile routing fix

--- books_for_you/settings.py
+++ books_for_you/settings.py
@@ -22,6 +22,7 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 
+DEBUG = True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
