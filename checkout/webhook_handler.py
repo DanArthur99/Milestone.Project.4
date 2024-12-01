518aa0e3ba571f4965237529cae586a9d0f16a2c
Dan Arthur <148535574+DanArthur99@users.noreply.github.com>
2024-12-01 05:06:30 +0000

    fix: duplicate order fix attempt

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -80,15 +80,13 @@ class StripeWH_Handler:
         attempt = 1
         try:
             order = Order.objects.get(
-                full_name__iexact=shipping_details.name,
-                email__iexact=email,
-                phone_number__iexact=shipping_details.phone,
-                country__iexact=shipping_details.address.country,
-                postcode__iexact=shipping_details.address.postal_code,
-                town_or_city__iexact=shipping_details.address.city,
-                street_address1__iexact=shipping_details.address.line1,
-                street_address2__iexact=shipping_details.address.line2,
-                county__iexact=shipping_details.address.state,
+                full_name=shipping_details.name,
+                email=email,
+                phone_number=shipping_details.phone,
+                country=shipping_details.address.country,
+                postcode=shipping_details.address.postal_code,
+                town_or_city=shipping_details.address.city,
+                street_address1=shipping_details.address.line1,
                 grand_total=grand_total,
                 original_bag=basket,
                 stripe_pid=pid,
