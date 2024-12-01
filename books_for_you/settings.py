4bf3799af65e691f0640652d1af5fcde340183db
Dan Arthur <148535574+DanArthur99@users.noreply.github.com>
2024-12-01 03:38:23 +0000

    added to send email config vars

--- books_for_you/settings.py
+++ books_for_you/settings.py
@@ -213,6 +213,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 if 'DATABASE_URL' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
+    EMAIL_USE_SSL = False
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
