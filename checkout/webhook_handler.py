9728856736ff73a2772a8020397a2e1837e7653e
Dan Arthur <148535574+DanArthur99@users.noreply.github.com>
2024-12-01 04:59:22 +0000

    fix: duplicate order fix attempt

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -78,27 +78,25 @@ class StripeWH_Handler:
 
         order_exists = False
         attempt = 1
-        while attempt <= 5:
-            try:
-                order = Order.objects.get(
-                    full_name__iexact=shipping_details.name,
-                    email__iexact=email,
-                    phone_number__iexact=shipping_details.phone,
-                    country__iexact=shipping_details.address.country,
-                    postcode__iexact=shipping_details.address.postal_code,
-                    town_or_city__iexact=shipping_details.address.city,
-                    street_address1__iexact=shipping_details.address.line1,
-                    street_address2__iexact=shipping_details.address.line2,
-                    county__iexact=shipping_details.address.state,
-                    grand_total=grand_total,
-                    original_bag=basket,
-                    stripe_pid=pid,
-                )
-                order_exists = True
-                break
-            except Order.DoesNotExist:
-                attempt += 1
-                time.sleep(1)
+        try:
+            order = Order.objects.get(
+                full_name__iexact=shipping_details.name,
+                email__iexact=email,
+                phone_number__iexact=shipping_details.phone,
+                country__iexact=shipping_details.address.country,
+                postcode__iexact=shipping_details.address.postal_code,
+                town_or_city__iexact=shipping_details.address.city,
+                street_address1__iexact=shipping_details.address.line1,
+                street_address2__iexact=shipping_details.address.line2,
+                county__iexact=shipping_details.address.state,
+                grand_total=grand_total,
+                original_bag=basket,
+                stripe_pid=pid,
+            )
+            order_exists = True
+        except Order.DoesNotExist:
+            attempt += 1
+            time.sleep(1)
         if order_exists:
             self._send_confirmation_email(order, email)
             return HttpResponse(
