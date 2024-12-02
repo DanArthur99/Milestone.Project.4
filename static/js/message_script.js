f18632a3162ee0c94fb0401c0be123a13057e7b4
Dan Arthur <148535574+DanArthur99@users.noreply.github.com>
2024-12-02 02:20:28 +0000

    alert removal functionality

---
+++ static/js/message_script.js
@@ -0,0 +1,12 @@
+$('.message-remover-danger').on('click', function() {
+    $('.danger-alert').css('display', 'none')
+})
+$('.message-remover-success').on('click', function() {
+    $('.success-alert').css('display', 'none')
+})
+$('.message-remover-info').on('click', function() {
+    $('.danger-info').css('display', 'none')
+})
+$('.message-remover-warning').on('click', function() {
+    $('.danger-warning').css('display', 'none')
+})
