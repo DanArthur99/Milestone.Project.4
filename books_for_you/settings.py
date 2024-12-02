370ff5a555db3de93510b2099c784b5f760c2719
Dan Arthur <148535574+DanArthur99@users.noreply.github.com>
2024-12-02 22:05:12 +0000

    debug test

--- books_for_you/settings.py
+++ books_for_you/settings.py
@@ -28,7 +28,7 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 
-DEBUG = False
+DEBUG = True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
